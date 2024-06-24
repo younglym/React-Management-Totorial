07dfb1846bcc638927788451338559c37b6d530c
younglym <groupware@hanmail.net>
2024-06-25 08:12:27 +0900

    Add Node.js Express Server Module

---
+++ client/server.js
@@ -0,0 +1,33 @@
+const bodyParser = require('body-parser');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+});
+
+connection.connect();
+
+app.get('/api/customers', (req, res) => {
+    connection.query(
+        "SELECT * FROM customer",
+        (err, rows, fields) => {
+            res.send(rows);
+        }
+    );
+
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -5,29 +6,8 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
-const data = fs.readFileSync('./database.json');
-const conf = JSON.parse(data);
-const mysql = require('mysql');
-
-const connection = mysql.createConnection({
-    host: conf.host,
-    user: conf.user,
-    password: conf.password,
-    port: conf.port,
-    database: conf.database
-});
-
-connection.connect();
-
-app.get('/api/customers', (req, res) => {
-    connection.query(
-        "SELECT * FROM customer",
-        (err, rows, fields) => {
-            res.send(rows);
-        }
-    );
-
+app.get('/api/hello', (req, res) => {
+    res.send({message: '안녕하세요!'});
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
-
